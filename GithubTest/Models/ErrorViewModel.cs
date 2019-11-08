4fb9a46a3fd8c844aa0ea68ac00c579a4662abcc
Chung Zheng Yang <czyang98@hotmail.com>
2019-11-08 11:23:42 +0800

    ok

---
+++ GithubTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GithubTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
