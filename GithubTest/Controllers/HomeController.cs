94bdeeb50b6a2ce662e1e7610de787352153c63c
Chung Zheng Yang <czyang98@hotmail.com>
2019-11-08 11:54:43 +0800

    okok

--- GithubTest/Controllers/HomeController.cs
+++ GithubTest/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace GithubTest.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page here. Test.";
+            ViewData["Message"] = "Your application description page here. Test ok.";
 
             return View();
         }
